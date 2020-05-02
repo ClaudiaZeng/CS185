475f6c949fe79bf3abb3f896bdece4de28dae317
Yuqing Zeng <claudia@Yuqings-MacBook-Pro.local>
2020-05-02 02:40:03 -0700

    Updates

---
+++ precache-manifest.229f29592a17099a32ba6080cc7f104a.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c227363c0763ef4fcb9a2a94567faca9",
+    "url": "/CS185/hw4app/index.html"
+  },
+  {
+    "revision": "41fcf66a0812f07ba421",
+    "url": "/CS185/hw4app/static/css/main.73620655.chunk.css"
+  },
+  {
+    "revision": "90f7898c15a3cec21879",
+    "url": "/CS185/hw4app/static/js/2.33d1dff9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CS185/hw4app/static/js/2.33d1dff9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41fcf66a0812f07ba421",
+    "url": "/CS185/hw4app/static/js/main.2b962571.chunk.js"
+  },
+  {
+    "revision": "a09b93b5b9f2ae788703",
+    "url": "/CS185/hw4app/static/js/runtime-main.1f6c7e6c.js"
+  },
+  {
+    "revision": "7a84b0f8d2a55482b6547e2528eedebd",
+    "url": "/CS185/hw4app/static/media/Cirque_du_Soleil_Logo.7a84b0f8.png"
+  },
+  {
+    "revision": "2854d66b4208a258d2e09c76b7d5e4b9",
+    "url": "/CS185/hw4app/static/media/ac1.2854d66b.jpeg"
+  },
+  {
+    "revision": "f6a941fed574e694695a127f322e1799",
+    "url": "/CS185/hw4app/static/media/ac2.f6a941fe.JPG"
+  },
+  {
+    "revision": "370e19680f96cfa958e8b97e6b6fbfd2",
+    "url": "/CS185/hw4app/static/media/cat.370e1968.mp4"
+  },
+  {
+    "revision": "4d45f760161ca925c14190c596fa93e6",
+    "url": "/CS185/hw4app/static/media/claudiazeng.4d45f760.png"
+  },
+  {
+    "revision": "1916464c40db41cef9815a3b55ca020b",
+    "url": "/CS185/hw4app/static/media/flowers.1916464c.JPG"
+  },
+  {
+    "revision": "e260d70e247de1b59bdd957a234d2d93",
+    "url": "/CS185/hw4app/static/media/food1.e260d70e.JPG"
+  },
+  {
+    "revision": "7c770f0100e3ba5ecdac5b2191c446cc",
+    "url": "/CS185/hw4app/static/media/food2.7c770f01.JPG"
+  },
+  {
+    "revision": "b4abf3089606e6310394863d5e1e16c2",
+    "url": "/CS185/hw4app/static/media/game1.b4abf308.mp4"
+  },
+  {
+    "revision": "31ff87a0dca503aac5ec7fc0ffee8ba4",
+    "url": "/CS185/hw4app/static/media/game2.31ff87a0.mp4"
+  },
+  {
+    "revision": "97a3202579d1cbdd04ca0cc792bcac67",
+    "url": "/CS185/hw4app/static/media/ny1.97a32025.JPG"
+  },
+  {
+    "revision": "ba06587e2b9162f1b8eca41e69c061c3",
+    "url": "/CS185/hw4app/static/media/ny2.ba06587e.JPG"
+  },
+  {
+    "revision": "59516f7250e3ae2fa0ab7e0be29bab46",
+    "url": "/CS185/hw4app/static/media/ny3.59516f72.JPG"
+  },
+  {
+    "revision": "48f63d4e354a92a3f795b2c40aae17bb",
+    "url": "/CS185/hw4app/static/media/orlando1.48f63d4e.JPG"
+  },
+  {
+    "revision": "77d83831c78008604744cbe1c6e763bb",
+    "url": "/CS185/hw4app/static/media/orlando2.77d83831.JPG"
+  },
+  {
+    "revision": "de1dafa7b34e7ff56e0be92b914e75f4",
+    "url": "/CS185/hw4app/static/media/orlando3.de1dafa7.JPG"
+  },
+  {
+    "revision": "36d2ac68ce91ba35e160e0773ab2164c",
+    "url": "/CS185/hw4app/static/media/paint1.36d2ac68.JPG"
+  },
+  {
+    "revision": "d206177b72affaad4cc8a57f52387454",
+    "url": "/CS185/hw4app/static/media/paint2.d206177b.JPG"
+  },
+  {
+    "revision": "899190b55ee84c7b94f6d39b95b81b14",
+    "url": "/CS185/hw4app/static/media/sb.899190b5.JPG"
+  },
+  {
+    "revision": "1edae245024b0cbd6de5dad96cfd5d44",
+    "url": "/CS185/hw4app/static/media/sbsunset.1edae245.mp4"
+  },
+  {
+    "revision": "130e356c210f6c5d2d5342b8ed9ffbcd",
+    "url": "/CS185/hw4app/static/media/seattle1.130e356c.JPG"
+  },
+  {
+    "revision": "2054672d79cc718f42305ebc5d83781f",
+    "url": "/CS185/hw4app/static/media/seattle2.2054672d.JPG"
+  },
+  {
+    "revision": "fb59b979bf347705c9e03d25118b7705",
+    "url": "/CS185/hw4app/static/media/spaceX.fb59b979.MP4"
+  }
+]);
